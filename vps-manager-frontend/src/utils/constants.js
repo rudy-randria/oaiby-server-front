5ebf0b9359f51529d7fb44ad7ad7cb33a5d30680
Rudy <rudrandria@gmail.com>
2025-08-24 08:52:34 +0300

    add - use api endpoint

--- vps-manager-frontend/src/utils/constants.js
+++ vps-manager-frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE = 'http://localhost:8000';
+export const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:8000';
 
 export const SERVICE_ACTIONS = {
   STATUS: 'status',
